a8d99ae2ef481bc9b29f2e705e31fedbd732b7d9
JoaoAlgeri <joaoalgeri@gmail.com>
2023-01-16 08:24:53 -0300

    Get participants adicionado

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import {MongoClient, ObjectId} from 'mongodb';
 import dotenv from 'dotenv';
+import dayjs from 'dayjs';
 
 dotenv.config();
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
@@ -11,10 +12,16 @@ const app = express();
 app.use(express.json());
 app.use(cors())
 
-app.get('/participants',(req,res)=>{
+app.get('/participants',async (req,res)=>{
+   const participants= await db.collection("users").find().toArray();
+
+    res.send(participants);
+})
+app.post('/participants',async(req,res)=>{
+    const {nome}=req.body;
+    await db.mongoClient.db()
     res.send("servidor funcionando corretamente")
 })
-
 app.listen(5000, () => {
     console.log('Servidor rodando na porta 5000')
   })
