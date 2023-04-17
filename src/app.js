1eaf27f86f2b3afe0737f66ca51c698b4582d7a6
JoaoAlgeri <joaoalgeri@gmail.com>
2023-04-17 16:04:16 -0300

    Fixing error

--- src/app.js
+++ src/app.js
@@ -6,51 +6,74 @@ import dayjs from 'dayjs'
 import joi from 'joi'
 
 dotenv.config()
-const app = express();
-app.use(express.json());
-app.use(cors);
 
 
-const userschema = joi.object({
-    name: joi.string().required().min(1)
+const userSchema = joi.object({
+    name: joi.string().min(3).required()
 })
-const mongoClient = new MongoClient(process.env.DATABASE_URL);
-let db;
-
-try {
-    await mongoClient.connect()
-    db = mongoClient.db()
-} catch (error) {
-    console.error(error)
-    console.log('Houve um problema no servidor, tente novamente mais tarde')
-}
-
-app.post("/participants",async (req, res) => {
-   
-    const { name } = req.body;
-    const participant = { name, lastStatus: Date.now() }
-    const message = {
-        from: name,
-        to: 'Todos',
-        text: 'entra na sala...',
-        type: 'status',
-        time: dayjs().format('HH:mm:ss')
-    }
-    const validation = userschema.validate(req.body);
-    if (validation.error) res.status(422).send
-    const participantIsLogged= await db.collection("participants").findOne({name})
+// const messageSchema=joi.object({
+    //     to:joi.string().required().min(1)
+    //     text:string.required().min(3)
+    //     type:string.required().min(3)
+    // })
+    const mongoClient = new MongoClient(process.env.DATABASE_URL);
+    let db;
+    
     try {
-       
-        await db.collection("participants").insertOne(participant)
-        await db.collection("messages").insertOne(message)
-        res.sendStatus(201);
+        await mongoClient.connect()
+        db = mongoClient.db()
+    } catch (error) {
+        console.error(error)
+        console.log('Houve um problema no servidor, tente novamente mais tarde')
+    }
+    const app = express();
+    app.use(express.json());
+    app.use(cors());
+    
+    app.post("/participants", async (req, res) => {
+        
+        const { name } = req.body;
+
+        const {error} = userSchema.validate({name});
+
+        if (error) res.status(422).send()
+        try {
+            const participantIsLogged = await db.collection("participants").findOne({ name })
+
+            if(participantIsLogged) return res.status(409).send("Este nome ja foi utilizado")
+
+   
+        await db.collection("participants").insertOne({ name, lastStatus: Date.now() })
+
+        await db.collection("messages").insertOne({
+            from: name,
+            to: 'Todos',
+            text: 'entra na sala...',
+            type: 'status',
+            time: dayjs().format('HH:mm:ss')
+        })
+
+        res.status(201).send("OK");
+        
     }
     catch (error) {
-        console.log(error);
+        
         res.status(500).send('Houve um problema no servidor, tente novamente mais tarde');
     }
+    app.get("/participants",async(req,res)=>{
+        try{
+            const participants= await db.collection("participants").find().toArray()
+            if(participants===undefined) res.status(404).send("Nenhum usuário online")
+            res.send(participants)
+        }
+        catch{
+            res.status(500).send('Houve um problema no servidor, tente novamente mais tarde')
+        }
+   
+ })
+ app.post("/messages",(req,res)=>{
 
-
+ })
 })
 
 const PORT = 5000;
